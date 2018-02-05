7c6a0c9ceee7c0f765dbba0616d688a8dfbb49e1
Bartłomiej Bajda <allerbbb@gmail.com>
2018-02-05 18:03:10 +0000

    fix script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,7 @@ function getTriangleArea (a, h) {
   if ((a > 0 ) && (h > 0)) {
     return (a*h/2);
   }
-  else {
     return ("Podales nieprawidlowe dane");
-  }
-
 }
 
 console.log(getTriangleArea(10, 6));
